d17c85e88af8b9ba8be8108b8bf186bd3088fcb1
Alex McLeod <alex@mcleod.io>
2016-04-03 16:36:17 -0700

    Reorganized package and added requirements.txt

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='django-simple-api',
+      version='0.1',
+      description='Builds on django-rest-framework to make developing a powerful Django API dead simple.',
+      url='https://github.com/Myagi/django-simple-api',
+      author='Alex McLeod',
+      author_email='alex.mcleod@myagi.com',
+      license='MIT',
+      packages=['simple-api'],
+      zip_safe=False)
